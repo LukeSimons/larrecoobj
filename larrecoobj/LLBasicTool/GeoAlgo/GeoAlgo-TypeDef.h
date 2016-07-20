670e6c162df990636191ef513875923e2b6919c0
Luke Simons <lsimons@woof.fnal.gov>
2016-07-20 13:12:58 -0500

    Initial Commit. Linker error with LLSelectionTool/OpT0Finder/Algorithms. Work in Progress -LS

---
+++ larrecoobj/LLBasicTool/GeoAlgo/GeoAlgo-TypeDef.h
@@ -0,0 +1,36 @@
+//
+// type-def file for ROOT
+//
+
+#ifndef SELECTIONTOOL_OPT0FINDER_BASE_TYPEDEF_H
+#define SELECTIONTOOL_OPT0FINDER_BASE_TYPEDEF_H
+
+// Header files included here
+#include "GeoAlgoConstants.h"
+#include "GeoAlgoException.h"
+#include "GeoVector.h"
+#include "GeoTrajectory.h"
+#include "GeoHalfLine.h"
+#include "GeoLine.h"
+#include "GeoLineSegment.h"
+#include "GeoAABox.h"
+#include "GeoAlgo.h"
+
+//ADD_NEW_HEADER ... do not change this comment line
+
+// Class forward declaration here
+namespace geoalgo {
+  class GeoAlgoException;
+  class Vector;
+  class Trajectory;
+  class HalfLine;
+  class Line;
+  class LineSegment;
+  class AABox;
+  class GeoAlgo;
+
+  class GeoObjCollection;
+}
+
+//ADD_EMPTY_CLASS ... do not change this comment line
+#endif
---
+++ larrecoobj/LLSelectionTool/OpT0Finder/Base/Base-TypeDef.h
@@ -0,0 +1,35 @@
+//
+// type-def file for ROOT
+//
+
+#ifndef BASICTOOL_GEOALGO_TYPEDEF_H
+#define BASICTOOL_GEOALGO_TYPEDEF_H
+
+// Header files included here
+#include "BaseAlgorithm.h"
+#include "BaseFlashFilter.h"
+#include "BaseFlashHypothesis.h"
+#include "BaseFlashMatch.h"
+#include "BaseProhibitAlgo.h"
+#include "BaseTPCFilter.h"
+#include "ColorPrint.h"
+#include "FlashMatchManager.h"
+#include "OpT0FinderException.h"
+
+//ADD_NEW_HEADER ... do not change this comment line
+
+// Class forward declaration here
+namespace geoalgo {
+  class OpT0FinderException;
+  class ColorPrint;
+  class FlashMatchManager;
+  class BaseAlgorithm;
+  class BaseFlashFilter;
+  class BaseFlashHypothesis;
+  class BaseFlashMatch;
+  class BaseProhibitAlgo;
+  class BaseTPCFilter;
+}
+
+//ADD_EMPTY_CLASS ... do not change this comment line
+#endif
